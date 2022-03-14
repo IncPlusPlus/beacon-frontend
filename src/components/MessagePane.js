841c7652c81abde5ed12a3758667114a2789a08c
Izzo <izzoy@wit.edu>
2022-03-14 10:42:40 -0400

    Added message input field

--- src/components/MessagePane.js
+++ src/components/MessagePane.js
@@ -3,10 +3,13 @@ import {Message} from './Message';
 
 export const MessagePane = (props) => {
     return (
-        <ul className='messagePane'>
-            {props.messages.map(
-                (msg) => <Message key={msg.id} message={msg}/>
-            )}
-        </ul>
+        <div className='messagePane'>
+            <ol>
+                {props.messages.map(
+                    (msg) => <Message key={msg.id} message={msg}/>
+                )}
+            </ol>
+            <input id="messageInput" type="text" placeholder='Message' className='messageInputField'/>
+        </div> 
     );
 }
