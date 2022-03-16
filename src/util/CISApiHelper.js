81b99de021ba42a53e60a9c9acb886059e34365d
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-03-16 17:41:50 -0400

    Switch the config to the QA URL

--- src/util/CISApiHelper.js
+++ src/util/CISApiHelper.js
@@ -1,15 +1,15 @@
 import {AccountManagementApi, CityManagementApi, Configuration} from "beacon-central-identity-server";
 
 // eslint-disable-next-line
+const LOCAL_URL = "http://localhost:9876";
+// eslint-disable-next-line
 const QA_URL = "https://beacon-cis-main-staging.herokuapp.com";
 // eslint-disable-next-line
 const PROD_URL = "https://beacon-cis-main-staging.herokuapp.com";
-// eslint-disable-next-line
-const LOCAL_URL = "http://localhost:9876";
 
 // Is of type ConfigurationParameters
 export const DefaultConfig = {
-    basePath: LOCAL_URL,
+    basePath: QA_URL,
 };
 
 export const AccountManagementHelper = new AccountManagementApi(new Configuration(DefaultConfig));
