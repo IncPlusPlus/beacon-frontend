d3be0a4e5c9f87fed2567cfa1f83e9d5213dc781
Yali Izzo <Sciman1011@Gmail.com>
2022-04-04 22:33:08 -0400

    Started working on sign up interface

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {observer} from "mobx-react-lite";
 import ReactMarkdown from 'react-markdown'
+import { UserContext } from '../context/userContext';
 
 export const Message = observer(function Message(props) {
+
+    const {getUserInfo} = useContext(UserContext);
+
     /*
      * For editing and deleting messages, we could either do what discord does with showing buttons on a message
      * on hover, or we could make a right click menu. Here's an example button to delete this message instance.
@@ -10,7 +14,7 @@ export const Message = observer(function Message(props) {
      */
     return (
         <div className='message'>
-            <strong>{props.message.senderId}</strong><br/>
+            <strong>{getUserInfo(props.message.senderId).username}</strong><br/>
             <span><ReactMarkdown>{props.message.messageBody}</ReactMarkdown></span>
         </div>
     );
