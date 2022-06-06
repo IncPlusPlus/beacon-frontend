1b5ff30e28d17cdbe54ded150cb1fbc6a3931dfb
Sciman101 <Sciman1011@Gmail.com>
2022-06-06 16:32:51 -0400

    Added delete message button

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,12 +2,12 @@ import React, {useContext} from 'react';
 import {observer} from "mobx-react-lite";
 import ReactMarkdown from 'react-markdown'
 import {UserContext} from '../context/userContext';
-import {TowerContext} from '../context/towerContext';
 import default_avatar from '../assets/default-avatar.png';
+import { SignInContext } from '../context/signInContext';
 
 export const Message = observer(function Message(props) {
     const {getUsername} = useContext(UserContext);
-    const {currentUsername} = useContext(TowerContext);
+    const {accountId} = useContext(SignInContext);
     /*
      * For editing and deleting messages, we could either do what discord does with showing buttons on a message
      * on hover, or we could make a right click menu. Here's an example button to delete this message instance.
@@ -15,14 +15,6 @@ export const Message = observer(function Message(props) {
      */
 
     const senderUsername = getUsername(props.message.senderId);
-
-    let messageActions = <></>;
-    if (currentUsername === senderUsername) {
-        messageActions = <span className='messageActions'>
-            Edit | Delete
-        </span>;
-    }
-
     return (
         <div className='message'>
 
@@ -33,7 +25,12 @@ export const Message = observer(function Message(props) {
                 <div className='messageContent'><ReactMarkdown>{props.message.messageBody}</ReactMarkdown></div>
             </span>
 
-            {messageActions}
+            {(props.message.senderId === accountId) && 
+                <span className='messageActionContainer'>
+                    {/*TODO: Add confirmation box for deletion*/}
+                    <span className='messageActionButton' onClick={() => props.message.deleteMessage() }>Delete</span>
+                </span>
+            }
         </div>
     );
 });
