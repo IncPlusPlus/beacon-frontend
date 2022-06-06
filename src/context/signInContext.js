d0a37fbc62d2f3608554dc113fb5fc6ae3d7c670
Sciman101 <Sciman1011@Gmail.com>
2022-06-06 16:13:40 -0400

    Update signInContext.js

--- src/context/signInContext.js
+++ src/context/signInContext.js
@@ -64,6 +64,7 @@ export function SignInContextProvider({children}) {
         setSignupState('');
         setCurrentUsername('');
         setCurrentPassword('');
+        setCurrentUserId('');
     };
 
     return (
