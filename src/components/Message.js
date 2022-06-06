a43654a26a5e79c11cc5c3791141dfd53e0197dc
Sciman101 <Sciman1011@Gmail.com>
2022-06-06 18:17:53 -0400

    Added message 'collapsing' and reformatted the message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,11 +3,9 @@ import {observer} from "mobx-react-lite";
 import ReactMarkdown from 'react-markdown'
 import {UserContext} from '../context/userContext';
 import default_avatar from '../assets/default-avatar.png';
-import { SignInContext } from '../context/signInContext';
 
 export const Message = observer(function Message(props) {
     const {getUsername} = useContext(UserContext);
-    const {accountId} = useContext(SignInContext);
     /*
      * For editing and deleting messages, we could either do what discord does with showing buttons on a message
      * on hover, or we could make a right click menu. Here's an example button to delete this message instance.
@@ -16,21 +14,14 @@ export const Message = observer(function Message(props) {
 
     const senderUsername = getUsername(props.message.senderId);
     return (
-        <div className='message'>
+        <div className={'message' + (props.minimal ? ' minimal' : '')}>
 
-            <img className='messageUserIcon' alt={senderUsername} src={default_avatar}/>
+            {!props.minimal && <img className='messageUserIcon' alt={senderUsername} src={default_avatar}/>}
 
             <span>
-                <div className='messageUsername'>{senderUsername}</div>
+                {!props.minimal && <div className='messageUsername'>{senderUsername}</div>}
                 <div className='messageContent'><ReactMarkdown>{props.message.messageBody}</ReactMarkdown></div>
             </span>
-
-            {(props.message.senderId === accountId) && 
-                <span className='messageActionContainer'>
-                    {/*TODO: Add confirmation box for deletion*/}
-                    <span className='messageActionButton' onClick={() => props.message.deleteMessage() }>Delete</span>
-                </span>
-            }
         </div>
     );
 });
